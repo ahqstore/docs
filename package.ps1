bd690c71769f3cb857ee51802ad978c0630086bc
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-09-21 15:29:57 +0530

    404

--- package.ps1
+++ package.ps1
@@ -4,3 +4,6 @@ New-Item docs -ItemType Directory
 
 New-Item docs/env/types -ItemType Directory
 Copy-Item .\client\src-tauri\plugin-script\types\docs\* -Destination .\docs\env\types -Recurse -Force
+
+Copy-Item .\overrides\index.html -Destination .\docs\index.html
+Copy-Item .\overrides\404.html -Destination .\docs\404.html
