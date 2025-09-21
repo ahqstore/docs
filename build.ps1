db71dc15e4ddf4f76986b6ad3bb82d18bf8550a6
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-09-21 14:29:46 +0530

    Fix publishing

--- build.ps1
+++ build.ps1
@@ -2,8 +2,6 @@ Remove-Item -Force -Recurse client -ErrorAction SilentlyContinue
 
 git clone "https://github.com/ahqstore/client.git"
 
-npm i -g pnpm
-
 # Go to types
 Set-Location .\client\src-tauri\plugin-script\
 
