6bce8775d1be17bbeb27452a799b4f17fd55631c
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-09-21 14:28:40 +0530

    Try to fix

--- build.ps1
+++ build.ps1
@@ -7,10 +7,10 @@ npm i -g pnpm
 # Go to types
 Set-Location .\client\src-tauri\plugin-script\
 
-pnpm install
-pnpm build:types
+npm install
+npm run build:types
 
 Set-Location .\types
 
-pnpm install
-pnpm build:docs
+npm install
+npm run build:docs
